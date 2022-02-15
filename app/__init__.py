75130721b2271725069e6fd2aa85d0c0ee62251a
Yag20 <yag.shukla@gmail.com>
2022-02-15 15:26:16 +0000

    login

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
-
-# @app.route('/')
-# def home(): 
-#     return 'Hello World!'
-
-# if __name__ == '__main__':
-#     app.run() 
